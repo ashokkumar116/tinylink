ae5f98619ca2c69663fa4a3b0d829c3b80db674d
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-11-18 23:43:20 +0530

    Modify POST Links endpoint to validate URL and generate code if not given

--- app/api/links/route.ts
+++ app/api/links/route.ts
@@ -1,34 +1,61 @@
 import { db } from "@/db";
 import { links } from "@/db/schema";
+import { generateCode } from "@/utils/generateCode";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
-export async function GET(request:Request){
-
-    const allLinks  = await db.select().from(links);
+export async function GET(request: Request) {
+    const allLinks = await db.select().from(links);
 
     return NextResponse.json({
-        links:allLinks[0]
-    })
+        links: allLinks[0],
+    });
 }
 
-export async function POST(req:Request){
-    const {code,targetUrl} : {code:string,targetUrl:string} = await req.json();
-    const alreadyAvailable = await db.select().from(links).where(eq(links.code,code));
-    if(alreadyAvailable.length > 0){
-        return NextResponse.json({
-            message:"link already exists"
-        },
-        {
-            status:409
-        })
+export async function POST(req: Request) {
+    let { code, targetUrl }: { code?: string; targetUrl: string } =
+        await req.json();
+    if (!targetUrl) {
+        return NextResponse.json(
+            {
+                message: "Target Url is required",
+            },
+            {
+                status: 400,
+            }
+        );
+    }
+    try {
+        new URL(targetUrl);
+    } catch {
+        return NextResponse.json({ message: "Invalid URL" }, { status: 400 });
+    }
+
+    if (!code || code.trim() === "") {
+        code = generateCode();
+    }
+
+    const alreadyAvailable = await db
+        .select()
+        .from(links)
+        .where(eq(links.code, code))
+        .limit(1);
+    if (alreadyAvailable.length > 0) {
+        return NextResponse.json(
+            {
+                message: "link already exists",
+            },
+            {
+                status: 409,
+            }
+        );
     }
     const res = await db.insert(links).values({
-        code:code,
-        targetUrl:targetUrl
-    })
+        code: code,
+        targetUrl: targetUrl,
+    });
     return NextResponse.json({
-        message:"success",
-        res
-    })
+        message: "success",
+        res,
+    });
 }
---
+++ utils/generateCode.ts
@@ -0,0 +1,13 @@
+export function generateCode(length: number = 6): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * characters.length);
+    result += characters[index];
+  }
+
+  return result;
+}
